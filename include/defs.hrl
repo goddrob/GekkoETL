d6271b9f1fd8a3fe5b44192807fb2f458eaf75e0
goddrob <robert.x90@gmail.com>
2013-10-31 21:22:07 +0100

    record definitions added

---
+++ include/defs.hrl
@@ -0,0 +1,4 @@
+-record(dailyStock, {name = "", symbol = "", date = "", currPrice = 0, changeValue = 0, changePercent = 0, prevClose = 0,
+ open = 0, high = 0, low = 0, volume = 0}).
+-record(histStock, {name = "", symbol = "", date = "", open = 0,
+ close = 0, high = 0, low = 0, volume = 0}).
