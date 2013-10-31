a01243ac2e3802a027df31f0becd2f785ff7309d
goddrob <robert.x90@gmail.com>
2013-10-31 22:31:12 +0100

    updated record description

--- include/defs.hrl
+++ include/defs.hrl
@@ -1,3 +1,4 @@
+%% date format in dailyStock record should be 19990320 20:00:00 as in YearMonthDay Hour:Minute:Second
 -record(dailyStock, {name = "", symbol = "", date = "", currPrice = 0, changeValue = 0, changePercent = 0, prevClose = 0,
  open = 0, high = 0, low = 0, volume = 0}).
 -record(histStock, {name = "", symbol = "", date = "", open = 0,
