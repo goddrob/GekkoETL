cb714610cdb0a4ef4c296f3d3274f4956e8b1e5b
daniKing <dani.hodovic@live.com>
2013-11-03 00:43:06 +0100

    Updated history stock records

--- include/defs.hrl
+++ include/defs.hrl
@@ -1,5 +1,6 @@
 %% date format in dailyStock record should be 19990320 20:00:00 as in YearMonthDay Hour:Minute:Second
+% Updated histStock/Dani
 -record(dailyStock, {name = "", symbol = "", date = "", currPrice = 0, changeValue = 0, changePercent = 0, prevClose = 0,
  open = 0, high = 0, low = 0, volume = 0}).
--record(histStock, {name = "", symbol = "", date = "", open = 0,
- close = 0, high = 0, low = 0, volume = 0}).
+-record(histStock, {symbol = "", date = "", open = "0",
+ close = "0", high = "0", low = "0", volume = "0"}).
