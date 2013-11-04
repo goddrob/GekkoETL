10505ce433762f8115087cb04277701c8e905be8
daniKing <dani.hodovic@live.com>
2013-11-04 14:24:58 +0100

    Signed-off-by: daniKing <dani.hodovic@live.com>

--- include/defs.hrl
+++ include/defs.hrl
@@ -1,6 +1,12 @@
-%% date format in dailyStock record should be 19990320 20:00:00 as in YearMonthDay Hour:Minute:Second
-% Updated histStock/Dani
+%% Date format in dailyStock record should be 19990320 20:00:00 as in YearMonthDay Hour:Minute:Second
+
+ % Record for daily stocks
 -record(dailyStock, {name = "", symbol = "", date = "", currPrice = 0, changeValue = 0, changePercent = 0, prevClose = 0,
  open = 0, high = 0, low = 0, volume = 0}).
+ 
+ % Record for the historical stock
 -record(histStock, {symbol = "", date = "", open = "0",
  close = "0", high = "0", low = "0", volume = "0"}).
+ 
+ % Record for the news
+-record(news {date = "", ticker = "", headline = "", url = ""}).
--- unused_code_src/parser_historical.erl
+++ unused_code_src/parser_historical.erl
@@ -1,38 +1,22 @@
-% Author : Dani Hodovic
-% Historical parser V2. Incomplete as it needs to be compatible with the database. 
-% For now it spawns mini-processes and recieves messages from those printing it in in the shell.
-
+% @AUTHOR: DANI HODOVIC
 -module(parser_historical).
--export([main/0]).
--record(rec, {symbol = "", date = "", open = "0", high = "0", low = "0", close = "0", volume = "0"}).
-
+% -export(main/0).
+-compile(export_all).
+-include("../include/defs.hrl").
+% NOT ClEANED, NOT COMPLETED, NOT COMMENTED, INITIAL VERSION
+% MISSING DEPENDENCIES, NOT CONCURRENT - V2 OUT SOON!
 
 main() ->
-	case whereis(server) of
-		undefined -> register(server, self());
-		_ -> registered_already
-	end,
 	inets:start(),
-	print(inets_started),
 	All_Tickers = nasdaqTickers:get(),
-	{A,_} = lists:split(2, All_Tickers),
-	print(process_starting),
-	iterate_tickers(A),
-	loop().
-	
+	{A,_} = lists:split(50, All_Tickers),
+	print(process_started),
+	lists:flatten(iterate_tickers(A, [])).
 
-loop() ->
-receive
-	M -> io:format("~p~n",[M]), loop()
-	after 3000 ->
-		timeout
-end.
-
-iterate_tickers([H|T]) ->
-	spawn_link(fun() -> processTicker(H) end),
-	iterate_tickers(T);
-iterate_tickers([]) ->
-	ok.
+iterate_tickers([H|T], Acc) ->
+	iterate_tickers(T, [processTicker(H)|Acc]);
+iterate_tickers([], Acc) ->
+	Acc.
 
 processTicker(Ticker) ->
 	{{Year, Month, Day}, _Time} = calendar:local_time(),
@@ -42,7 +26,7 @@ processTicker(Ticker) ->
 	S = "http://ichart.yahoo.com/table.csv?s="++Ticker
 		++"&a=5"++"&b=15"++"&c=2013"++"&d="++ SMonth ++ "&e="++ SDay ++ "&f="++ SYear
 			++"&d=m&ignore=.csv",
-	CSV = httpQ:get(S),
+	{_, {_,_,CSV}} = httpc:request(S),
 	case (string:chr(CSV, $!) > 0) of 
 		false -> parse_csv({CSV}, Ticker);
 		true -> error
@@ -50,30 +34,30 @@ processTicker(Ticker) ->
 
 parse_csv(CSV, Ticker) ->
 	[_|List] = re:split(tuple_to_list(CSV), "\n",[{return,list},{parts,infinity}]),
-	server ! {iterate_records(List, [], Ticker)}.
+	iterate_records(List, [], Ticker).
 
 % Iterates over records and calls another function 
 % to make the actualy records for each line	
 iterate_records([H|T], Acc, Ticker) ->
 	String = string:tokens(H, ","),
 	case String =/= [] of
-	 true -> iterate_records(T, [make_rec(String, 0, #rec{}, Ticker)|Acc], Ticker);
+	 true -> iterate_records(T, [make_rec(String, 0, #histStock{}, Ticker)|Acc], Ticker);
 	 false -> Acc
 	end;
 iterate_records([], Acc, _Ticker) -> Acc.
 
 make_rec([H|T], N, Rec, Ticker) -> 
-	make_rec(T, N + 1, Rec#rec{symbol = Ticker, date=((H -- [$-]) --[$-])}).
+	make_rec(T, N + 1, Rec#histStock{symbol = Ticker, date=((H -- [$-]) --[$-])}).
 make_rec([H|T], N, Rec) when N == 1 -> 
-	make_rec(T, N + 1, Rec#rec{open=H});
+	make_rec(T, N + 1, Rec#histStock{open=H});
 make_rec([H|T], N, Rec) when N == 2 -> 
-	make_rec(T, N + 1, Rec#rec{high=H});
+	make_rec(T, N + 1, Rec#histStock{high=H});
 make_rec([H|T], N, Rec) when N == 3 -> 
-	make_rec(T, N + 1, Rec#rec{low=H});
+	make_rec(T, N + 1, Rec#histStock{low=H});
 make_rec([H|T], N, Rec) when N == 4 -> 
-	make_rec(T, N + 1, Rec#rec{close=H});
+	make_rec(T, N + 1, Rec#histStock{close=H});
 make_rec([H|T], N, Rec) when N == 5 -> 
-	make_rec(T, N + 1, Rec#rec{volume=H});
+	make_rec(T, N + 1, Rec#histStock{volume=H});
 make_rec(_, _, Rec) ->
 	Rec.
 
@@ -82,7 +66,7 @@ print(Var) ->
 	io:format("*************"),
 	io:format("~p", [Var]),
 	io:format("*************~n").
+	
 
 % {531042000, 1000 list, 1 process}
-%  32745000
-% 38417000
+%  
