f4d60f6d62294fc35697747e0b5a1606aa77fe21
goddrob <robert.x90@gmail.com>
2013-12-02 19:16:34 +0100

    changed

--- src/server_sql.erl
+++ src/server_sql.erl
@@ -34,25 +34,6 @@ call_daily([],NewList,_) -> gen_server:cast(whereis(serverSQL),{daily,NewList});
 call_daily([H|T],NewList,Inc) ->
 	call_daily(T,[H|NewList],Inc+1).
 	
-%% Call for historical entries	
-call_hist(List) -> 
-	call_hist(List,[],0).
-call_hist(List,NewList,100) ->
-	gen_server:cast(whereis(serverSQL),{historical,NewList}),
-	call_hist(List,[],0);
-call_hist([],NewList,_) -> gen_server:cast(whereis(serverSQL),{historical,NewList});
-call_hist([H|T],NewList,Inc) -> call_hist(T,[H|NewList],Inc+1).
-
-%% Call for news entries
-call_news(List) ->
-	call_news(List,[],0).
-call_news(List,NewList,20) ->
-	gen_server:cast(whereis(serverSQL),{news,NewList}),
-	call_news(List,[],0);
-call_news([],NewList,_) -> gen_server:cast(whereis(serverSQL),{news,NewList});
-call_news([H|T],NewList,Inc) ->
-	call_news(T,[H|NewList],Inc+1).
-	
 %% Stops sql gen server
 stop() ->
 	gen_server:call(whereis(serverSQL),stop).
@@ -67,24 +48,12 @@ handle_call(stop, _From, State) ->
 	odbc:stop(),
 	{stop, normal, {sql,shutdown_ok}, State}.
 	
-handle_cast({news,RecList}, State) ->
-	Query = gen_query(news,RecList),
-	{ok,Pid} = odbc:connect(?ConnectStr,[]),
-	odbc:sql_query(Pid,Query),
-	odbc:disconnect(Pid),
-   {noreply, State};
 handle_cast({daily,RecList}, State) ->
 	Query = gen_query(daily,RecList),
 	{ok,Pid} = odbc:connect(?ConnectStr,[]),
 	odbc:sql_query(Pid,Query),
 	odbc:disconnect(Pid),
    {noreply, State};
-handle_cast({historical,RecList}, State) ->
-	Query = gen_query(historical,RecList),
-	{ok,Pid} = odbc:connect(?ConnectStr,[{timeout, 200000}]),
-	odbc:sql_query(Pid,Query),
-	odbc:disconnect(Pid),
-	{noreply, State}.
   
   
   
@@ -104,5 +73,3 @@ gen_query(_,[],Query) -> Query;
 gen_query(Type,[H|T],Query) -> gen_query(Type,T,Query++gen_entry(Type,H)++";").
 
 gen_entry(daily,R) -> lists:flatten(io_lib:format("EXEC s_addDaily @Name='"++R#dailyStock.name++"',@Symbol='"++R#dailyStock.symbol++"',@Datetime='"++R#dailyStock.date++"',@CurrentPrice=~p,@ChangeValue=~p,@ChangePercent=~p,@PreviousClose=~p,@Open=~p,@DayMaxPrice=~p,@DayMinPrice=~p,@Volume=~p",[R#dailyStock.currPrice,R#dailyStock.changeValue,R#dailyStock.changePercent,R#dailyStock.prevClose,R#dailyStock.open,R#dailyStock.high,R#dailyStock.low,R#dailyStock.volume]));
-gen_entry(historical,R) -> "EXEC s_addHistorical @Symbol='"++R#hist_stock.symbol++"',@Date='"++R#hist_stock.date++"',@Open="++R#hist_stock.open++",@Close="++R#hist_stock.close++",@MaxPrice="++R#hist_stock.high++",@MinPrice="++R#hist_stock.low++",@Volume="++R#hist_stock.volume;
-gen_entry(news,R) -> "EXEC s_addNews @Date='"++R#news.date++"',@Symbol='"++R#news.ticker++"',@Headline='"++R#news.headline++"',@Hyperlink='"++R#news.url++"'".
